5cbec74fd9ee3ddd423360ce531f7f278f6ba6b8
Gunter Hager <gupi@me.com>
2022-01-28 12:35:18 +0100

    Improved border test

--- emulator/mz800/XCTests/assets/TestBorder.asm
+++ emulator/mz800/XCTests/assets/TestBorder.asm
@@ -8,9 +8,52 @@ include "MZ800.inc"
 
 org 02000h
 
-  ; Set border
+; -----------------
+; Set stack
+  ld hl, StackStart ; stack goes down from stack start
+  ld sp, hl
+
+; -----------------
+; Main loop
+main:
+  ld a, ColorLightBlue
+  call set_border
+  call wait
+
+  ld a, ColorYellow
+  call set_border
+  call wait
+
+  ld a, ColorPurple
+  call set_border
+  call wait
+  jr main
+
+; -----------------
+; Set border
+; a contains color
+set_border:
   ld b, PortBorderColorHigh ; b contains the upper part of the port address
   ld c, PortBorderColorLow ; c contains the lower part of the port address
-  ld a, ColorLightBlue
   out (c), a
-  halt
+  ret
+
+; -----------------
+; Wait loop
+wait:
+  ld bc, 1000h
+outer:
+  ld de, 10h
+inner:
+  dec de
+  ld a, d
+  or e
+  jr nz, inner
+  dec bc
+  ld a, b
+  or c
+  jr nz, outer
+  ret
+
+  defs 256 ; room for stack
+StackStart:
